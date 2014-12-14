3b36274b97152c07fcdddb8334e45239a0673692
Ivan Nikolić <niksy5@gmail.com>
2014-12-14 19:36:16 +0100

    Restructure code

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function ( grunt ) {
 					banner: '<%= meta.banner %>'
 				},
 				files: {
-					'dist/kist-beacon.js': ['src/kist-beacon.js']
+					'dist/<%= pkg.name %>.js': ['compiled/<%= pkg.main %>']
 				}
 			}
 		},
@@ -26,7 +26,7 @@ module.exports = function ( grunt ) {
 					banner: '<%= meta.banner %>'
 				},
 				files: {
-					'dist/kist-beacon.min.js': ['src/kist-beacon.js']
+					'dist/<%= pkg.name %>.min.js': ['compiled/<%= pkg.main %>']
 				}
 			}
 		},
@@ -52,7 +52,8 @@ module.exports = function ( grunt ) {
 				},
 				files: {
 					src: [
-						'src/**/*.js'
+						'<%= pkg.main %>',
+						'lib/**/*.js'
 					]
 				}
 			}
@@ -64,23 +65,103 @@ module.exports = function ( grunt ) {
 					jshintrc: '.jshintrc'
 				},
 				src: [
-					'src/**/*.js'
+					'<%= pkg.main %>',
+					'lib/**/*.js'
 				]
 			}
-		}
+		},
+
+		browserify: {
+			options: {
+				browserifyOptions: {
+					standalone: 'kist.Beacon'
+				}
+			},
+			standalone: {
+				options: {
+					plugin: ['bundle-collapser/plugin']
+				},
+				files: {
+					'compiled/<%= pkg.main %>': ['<%= pkg.main %>']
+				}
+			}
+		},
+
+		nodemon: {
+			test: {
+				options: {
+					callback: function ( nodemon ) {
+						nodemon.on('config:update', function () {
+							setTimeout(function() {
+								require('opn')('http://0.0.0.0:3000/log');
+								require('opn')('http://0.0.0.0:3000/log1');
+							}, 1000);
+						});
+					}
+				},
+				script: 'test/server/server.js'
+			}
+		},
+
+		connect: {
+			test:{
+				options: {
+					open: true
+				}
+			}
+		},
+
+		'compile-handlebars': {
+			test: {
+				templateData: {
+					bower: '../../../bower_components',
+					compiled: '../../../compiled',
+					assets: 'assets',
+					main: '<%= pkg.main %>'
+				},
+				partials: 'test/manual/templates/partials/**/*.hbs',
+				template: 'test/manual/templates/*.hbs',
+				output: 'compiled/test/manual/*.html'
+			}
+		},
+
+		concurrent: {
+			options: {
+				logConcurrentOutput: true
+			},
+			test: ['watch','nodemon:test','connect:test:keepalive']
+		},
+
+		watch: {
+			options: {
+				spawn: false
+			},
+			hbs: {
+				files: 'test/manual/**/*.hbs',
+				tasks: ['compile-handlebars:test']
+			},
+			browserify: {
+				files: ['<%= pkg.main %>', 'lib/**/*.js'],
+				tasks: ['browserify:standalone']
+			}
+		},
 
 	});
 
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-jscs-checker' );
-	grunt.loadNpmTasks( 'grunt-contrib-concat' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-bump' );
+	require('load-grunt-tasks')(grunt);
 
-	grunt.registerTask( 'stylecheck', ['jshint:main', 'jscs:main'] );
-	grunt.registerTask( 'default', ['stylecheck', 'concat', 'uglify'] );
-	grunt.registerTask( 'releasePatch', ['bump-only:patch', 'default', 'bump-commit'] );
-	grunt.registerTask( 'releaseMinor', ['bump-only:minor', 'default', 'bump-commit'] );
-	grunt.registerTask( 'releaseMajor', ['bump-only:major', 'default', 'bump-commit'] );
+	grunt.registerTask('test', function () {
+		var tasks = ['compile-handlebars:test','browserify:standalone'];
+		if ( grunt.option('watch') ) {
+			tasks.push('concurrent:test');
+		}
+		grunt.task.run(tasks);
+	});
+
+	grunt.registerTask('stylecheck', ['jshint:main', 'jscs:main']);
+	grunt.registerTask('default', ['stylecheck', 'browserify:standalone', 'concat', 'uglify']);
+	grunt.registerTask('releasePatch', ['bump-only:patch', 'default', 'bump-commit']);
+	grunt.registerTask('releaseMinor', ['bump-only:minor', 'default', 'bump-commit']);
+	grunt.registerTask('releaseMajor', ['bump-only:major', 'default', 'bump-commit']);
 
 };
