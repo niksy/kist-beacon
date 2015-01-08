d97bb7ca1338a22f6fd20548af37c38ef5716e3b
Ivan Nikolić <niksy5@gmail.com>
2015-01-08 23:05:43 +0100

    Release 0.3.0

--- dist/kist-beacon.js
+++ dist/kist-beacon.js
@@ -1,13 +1,13 @@
-/*! kist-beacon 0.2.4 - Front-end logging interface. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2014 | License: MIT */
+/*! kist-beacon 0.3.0 - Front-end logging interface. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2015 | License: MIT */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),(f.kist||(f.kist={})).Beacon=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 /* jshint latedef:false */
 
 var indexof = require(5);
-var qs = require(6).stringify;
 var extend = require(8);
 var now = require(4);
 var patch = require(2);
+var pinghome = require(6);
 
 var consoleIntercepted = false;
 var queue = {
@@ -136,15 +136,9 @@ Beacon.prototype.defaults = {
 
 /**
  * @param  {Object} data
- *
- * @return {Object}
  */
 Beacon.prototype.send = function ( data ) {
-	var xhr = global.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-	xhr.open('POST', this.options.url, true);
-	xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-	xhr.send(qs(generate(data)));
-	return data;
+	pinghome(this.options.url, generate(data));
 };
 
 Beacon.prototype.destroy = function () {
@@ -227,70 +221,132 @@ module.exports = function(arr, obj){
   return -1;
 };
 },{}],6:[function(require,module,exports){
+(function (global){
+var qs = require(7);
 
 /**
- * Module dependencies.
- */
-
-var trim = require(7);
-
-/**
- * Parse the given query `str`.
+ * @param  {Object} data
+ * @param  {Boolean} useQs
  *
- * @param {String} str
- * @return {Object}
- * @api public
+ * @return {String|FormData}
  */
-
-exports.parse = function(str){
-  if ('string' != typeof str) return {};
-
-  str = trim(str);
-  if ('' == str) return {};
-
-  var obj = {};
-  var pairs = str.split('&');
-  for (var i = 0; i < pairs.length; i++) {
-    var parts = pairs[i].split('=');
-    obj[parts[0]] = null == parts[1]
-      ? ''
-      : decodeURIComponent(parts[1]);
-  }
-
-  return obj;
-};
-
-/**
- * Stringify the given `obj`.
- *
- * @param {Object} obj
- * @return {String}
- * @api public
- */
-
-exports.stringify = function(obj){
-  if (!obj) return '';
-  var pairs = [];
-  for (var key in obj) {
-    pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
-  }
-  return pairs.join('&');
-};
-
-},{}],7:[function(require,module,exports){
-
-exports = module.exports = trim;
-
-function trim(str){
-  return str.replace(/^\s*|\s*$/g, '');
+function prepare ( data, useQs ) {
+	var ret;
+	if ( !('FormData' in global) || useQs ) {
+		ret = qs(data);
+	} else {
+		ret = new FormData();
+		for ( var prop in data ) {
+			if ( data.hasOwnProperty(prop) ) {
+				ret.append(prop, data[prop]);
+			}
+		}
+	}
+	return ret;
 }
 
-exports.left = function(str){
-  return str.replace(/^\s*/, '');
+/**
+ * @param  {String} url
+ * @param  {Object} data
+ * @param  {Boolean} useSync
+ */
+module.exports = function ( url, data, useSync ) {
+	if ( navigator.sendBeacon ) {
+		navigator.sendBeacon(url, prepare(data));
+	} else {
+		var xhr = ('XMLHttpRequest' in global) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+		xhr.open('POST', url, (useSync ? false : true));
+		xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		xhr.send(prepare(data, true));
+	}
 };
 
-exports.right = function(str){
-  return str.replace(/\s*$/, '');
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}],7:[function(require,module,exports){
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+
+var stringifyPrimitive = function(v) {
+  switch (typeof v) {
+    case 'string':
+      return v;
+
+    case 'boolean':
+      return v ? 'true' : 'false';
+
+    case 'number':
+      return isFinite(v) ? v : '';
+
+    default:
+      return '';
+  }
+};
+
+module.exports = function(obj, sep, eq, name) {
+  sep = sep || '&';
+  eq = eq || '=';
+  if (obj === null) {
+    obj = undefined;
+  }
+
+  if (typeof obj === 'object') {
+    return map(objectKeys(obj), function(k) {
+      var ks = encodeURIComponent(stringifyPrimitive(k)) + eq;
+      if (isArray(obj[k])) {
+        return map(obj[k], function(v) {
+          return ks + encodeURIComponent(stringifyPrimitive(v));
+        }).join(sep);
+      } else {
+        return ks + encodeURIComponent(stringifyPrimitive(obj[k]));
+      }
+    }).join(sep);
+
+  }
+
+  if (!name) return '';
+  return encodeURIComponent(stringifyPrimitive(name)) + eq +
+         encodeURIComponent(stringifyPrimitive(obj));
+};
+
+var isArray = Array.isArray || function (xs) {
+  return Object.prototype.toString.call(xs) === '[object Array]';
+};
+
+function map (xs, f) {
+  if (xs.map) return xs.map(f);
+  var res = [];
+  for (var i = 0; i < xs.length; i++) {
+    res.push(f(xs[i], i));
+  }
+  return res;
+}
+
+var objectKeys = Object.keys || function (obj) {
+  var res = [];
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) res.push(key);
+  }
+  return res;
 };
 
 },{}],8:[function(require,module,exports){
